f3693e4bfe1c12e3de165fde7b55a965a7cfea36
Seán Burke <119906674+SBUplakankus@users.noreply.github.com>
2024-10-18 14:53:10 +0100

    Choice System

--- Assets/Scripts/Player/GameController.cs
+++ Assets/Scripts/Player/GameController.cs
@@ -1,14 +1,23 @@
 using System;
 using Dialogue;
 using PrimeTween;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Player
 {
     public class GameController : MonoBehaviour
     {
+        private enum ChoiceMade {Option1, Option2}
+        
         public PlayerController player;
         public DialogueController dialogue;
+        public ChoiceController choice;
+
+        private ChoiceMade _orderChoice;
+        private ChoiceMade _drinkChoice;
+        private ChoiceMade _meetingChoice;
+        
 
         private int _gameStage;
         
@@ -22,12 +31,14 @@ namespace Player
         {
             DialogueController.OnDialogueEnd += ProgressGame;
             PlayerController.OnMovementEnd += ProgressGame;
+            ChoiceController.OnChoiceMade += HandleChoiceMade;
         }
 
         private void OnDisable()
         {
             DialogueController.OnDialogueEnd -= ProgressGame;
             DialogueController.OnDialogueEnd -= ProgressGame;
+            ChoiceController.OnChoiceMade -= HandleChoiceMade;
         }
 
         private void ProgressGame()
@@ -49,10 +60,36 @@ namespace Player
                 case 4:
                     EventFive();
                     break;
+                case 5:
+                    switch (_orderChoice)
+                    {
+                        case ChoiceMade.Option1:
+                            GetShot();
+                            break;
+                        case ChoiceMade.Option2:
+                            EventSix();
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
             }
 
             _gameStage++;
-            Debug.Log(_gameStage);
+        }
+
+        private void HandleChoiceMade(int choice)
+        {
+            switch (_gameStage)
+            {
+                case 6:
+                    if (choice == 0)
+                        _orderChoice = ChoiceMade.Option1;
+                    else
+                        _orderChoice = ChoiceMade.Option2;
+                    break;
+            }
+            ProgressGame();
         }
         
         private void EventOne()
@@ -79,6 +116,22 @@ namespace Player
         private void EventFive()
         {
             player.LookAtTarget(1);
+            choice.SetChoices("What will you order?", "Doner Kebab", "Garlic Cheese Chips");
+        }
+
+        private void EventSix()
+        {
+            dialogue.StartConversation(2);
+        }
+
+        private void GetShot()
+        {
+            dialogue.StartConversation(3);
+        }
+        
+        private void EventSeven()
+        {
+            
         }
     }
 }
